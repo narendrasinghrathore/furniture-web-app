09025889026056803b023e245c6e00aea1870e5d
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2020-02-28 16:39:56 +0530

    For loading csv, tsv and xml file in project

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,20 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        test: /.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
       },
       {
-        test: /.(woff|woff2|eot|ttf|otf)$/,
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
       }
     ]
   }
