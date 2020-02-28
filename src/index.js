7468a181be42d34cc05e445bb51d18d0610b4c67
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2020-02-28 09:07:37 +0530

    Adding image loader webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './style.css';
-const element  = document.getElementById('app');
-element.classList.add('app');
-element.innerHTML = "<h1>Hello there.</h1>";
+import "./style.css";
+import icon from "../assets/svgs/empty-cart.svg";
+const element = document.getElementById("app");
+element.classList.add("app");
+element.innerHTML = `<h1>Hello there.</h1><img src='${icon}' />`;
