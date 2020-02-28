ad9f3199bced1f2b8012993fe246f50908e6619e
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2020-02-28 16:33:58 +0530

    Loading custom fonts ussing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
       {
         test: /.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
       }
     ]
   }
